96d07b10cc5b83a5b352798c858f607bfcd5ddb6
Jovan Jevtic <jovanjevtic132@gmail.com>
2024-05-29 18:53:05 +0200

    redis aws

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,75 +17,75 @@ app.use(morgan("dev"));
 
 app.use("/api/v1", v1);
 
-import * as https from 'https';
+import * as https from "https";
+import Redis from "ioredis";
 
 export const getPublicIp = (retries: number = 3): void => {
   const options = {
-    timeout: 5000 // 5 seconds timeout
+    timeout: 5000, // 5 seconds timeout
   };
 
-  const req = https.get('https://api.ipify.org?format=json', options, (resp) => {
-    let data = '';
+  const req = https.get(
+    "https://api.ipify.org?format=json",
+    options,
+    (resp) => {
+      let data = "";
 
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
 
-    resp.on('end', () => {
-      try {
-        // Provera sadržaja odgovora~
-        if (resp.headers['content-type']?.includes('application/json')) {
-          const jsonResponse = JSON.parse(data);
-          console.log(`Your public IP address is: ${jsonResponse.ip}`);
-        } else {
-          console.error('Unexpected response format:', data);
+      resp.on("end", () => {
+        try {
+          // Provera sadržaja odgovora~
+          if (resp.headers["content-type"]?.includes("application/json")) {
+            const jsonResponse = JSON.parse(data);
+            console.log(`Your public IP address is: ${jsonResponse.ip}`);
+          } else {
+            console.error("Unexpected response format:", data);
+          }
+        } catch (error) {
+          console.error("Error parsing JSON response:", error);
         }
-      } catch (error) {
-        console.error('Error parsing JSON response:', error);
-      }
-    });
-  });
+      });
+    }
+  );
 
   req.on("error", (err: any) => {
-    if (err.code === 'ECONNRESET' && retries > 0) {
-      console.log(`Connection reset by peer, retrying... (${retries} retries left)`);
+    if (err.code === "ECONNRESET" && retries > 0) {
+      console.log(
+        `Connection reset by peer, retrying... (${retries} retries left)`
+      );
       getPublicIp(retries - 1);
     } else {
       console.log("Error: " + err.message);
     }
   });
 
-  req.on('timeout', () => {
+  req.on("timeout", () => {
     req.abort();
-    console.log('Request timed out');
+    console.log("Request timed out");
   });
-}
+};
 
-import Redis from 'ioredis';
-// Konfiguracija za povezivanje sa Redis serverom
 const redis = new Redis({
   host: env.REDIS_IP,
   family: 6,
-  password: env.REDIS_PWD
-});
-// Provera da li je uspešno povezan sa Redis serverom
-redis.on('connect', () => {
-  console.log('Connected to Redis server');
+  password: env.REDIS_PWD,
 });
 
-// Slanje komande Redis serveru (npr. SET, GET)
-redis.set('key', 'value')
-  .then(() => console.log('Value set in Redis'))
-  .catch(err => console.error('Error setting value in Redis:', err));
+redis.on("connect", () => {
+  console.log("Connected to Redis server");
+});
+redis.on("error", (err) => {
+  console.log("Redis error", err);
+  // Optionally, implement a retry mechanism or alerting
+});
 
-// Dohvatanje vrednosti iz Redis servera
-redis.get('key')
-  .then(result => console.log('Value retrieved from Redis:', result))
-  .catch(err => console.error('Error getting value from Redis:', err));
-
-// Zatvaranje veze sa Redis serverom kada više nije potrebno
-// redis.quit();
-
+redis
+  .get("key")
+  .then((result) => console.log("Value retrieved from Redis:", result))
+  .catch((err) => console.error("Error getting value from Redis:", err));
 
 app.use(errorHandler);
 
